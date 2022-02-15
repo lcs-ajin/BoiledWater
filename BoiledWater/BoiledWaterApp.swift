9e507817ce9846b6ce702b74b4229fb2419f3b09
Anthony Jin <ajin@lcs.0n.ca>
2022-02-15 11:16:56 -0500

    Initial Commit

---
+++ BoiledWater/BoiledWaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoiledWaterApp.swift
+//  BoiledWater
+//
+//  Created by Anthony Jin on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct BoiledWaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
