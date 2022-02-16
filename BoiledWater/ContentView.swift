fcd7006e60b7764eb89f7025be82381eb4595d75
Anthony Jin <ajin@lcs.0n.ca>
2022-02-16 14:40:35 -0500

    Finish my project by adding the final two outputs and a navigation title

--- BoiledWater/ContentView.swift
+++ BoiledWater/ContentView.swift
@@ -8,11 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var B = Int.random(in: 80...120)
+    @State var B = 140.0
     
     
-    var P : Int{
-        return 5 * B - 400
+    var P : Double{
+        return Double(5 * B - 400)
     }
     var feedback: String{
         switch P {
@@ -28,19 +28,52 @@ struct ContentView: View {
    
     var body: some View {
         VStack{
+            
             HStack{
+                
              Text("Temperature")
                     .bold()
                     Spacer()
                 
             }
+            Slider(value:
+                $B,
+                   in: Double(Int(80.0))...Double(Int(200.0)),
+                   step: Double.Stride(Int.Stride(1.0)),
+                   label: {
+                Text("Temperature")
+            },
+                   minimumValueLabel: {
+                Text("80.0")
+            },
+                   maximumValueLabel: {
+                Text("200.0")
+            })
+            Text("\(String(format: "%.1f",B)) °C")
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            HStack {
+                Text("Atmosphric Pressure")
+                    .bold()
+                
+                Spacer()
+            }
+            Text("\(String(format: "%.1f", P)) Kpa")
+            Text(feedback)
+            
+            Spacer()
         }
+        .padding()
+        .navigationTitle("Boiled Water")
                 
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
